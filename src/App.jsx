0828dde1ad81310ebd921d0057d4553418435ee7
macklark <ganesh.kolavennu@gmail.com>
2021-09-11 14:57:58 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const title = "Hello, world!!";
+
+  return (
+    <div className="App">
+      {/* Text interpolation */}
+      <p>{title}</p>
+    </div>
+  );
+}
+
+export default App;
